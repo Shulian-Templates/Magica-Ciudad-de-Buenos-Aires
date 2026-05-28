2633190e4f29a7e0d970ab671778bd114294bd98
JZylber <zylberjulian@gmail.com>
2026-05-28 09:22:12 -0300

    fix: reset BDD

--- main.js
+++ main.js
@@ -1,47 +1,41 @@
-import pg from 'pg';
-import { resetearBDD } from './resetearBDD.js';
+import pg from "pg";
+import { resetearBDD } from "./resetearBDD.js";
 const { Client } = pg;
 
 const client = new Client({
-  host: 'DB_HOST',
+  host: "DB_HOST",
   port: 5432,
-  database: 'DB_NAME',
-  user: 'DB_OWNER',
-  password: 'DB_PASSWORD',
+  database: "DB_NAME",
+  user: "DB_OWNER",
+  password: "DB_PASSWORD",
   ssl: { rejectUnauthorized: false },
-  channelBinding: 'require',
+  channelBinding: "require",
 });
 
+async function getEventosDelDia(fecha) {}
 
-// Resetar/Cargar la BDD
-//await resetearBDD(client);
+async function borrarEventosPorLugar(lugar) {}
 
-async function getEventosDelDia(fecha) {
-}
+async function modificarAforo(evento, nuevoAforo) {}
 
-async function borrarEventosPorLugar(lugar) {
-}
+async function duplicarEventoMasAforo() {}
 
-async function modificarAforo(evento, nuevoAforo) {
-}
-
-async function duplicarEventoMasAforo() {
-}
-
-async function getBarriosMasEventos() {
-}
+async function getBarriosMasEventos() {}
 
 // Abro conexion a BD
 await client.connect();
 
+// Resetar/Cargar la BDD
+//await resetearBDD(client);
+
 // 1. Listado de eventos del dia 2024-01-06
-const eventos =await getEventosDelDia('2024-01-06')
+const eventos = await getEventosDelDia("2024-01-06");
 
 // 2. Borrar todos los eventos de Parque de la Ciudad
-const result = await borrarEventosPorLugar('Parque de la Ciudad')
+const result = await borrarEventosPorLugar("Parque de la Ciudad");
 
 // 3. Modificar el aforo del Burger Fest a 4000
-const result2 = await modificarAforo('Burger Fest', 4000)
+const result2 = await modificarAforo("Burger Fest", 4000);
 
 // 4a. Obtener el evento con mas aforo
 // 4b. Insertar ese evento con fecha de hoy
